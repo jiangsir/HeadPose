ce056a1734522ac7a5969c5bb67c6ea9d5fd43e7
jiangsir <jiangzero@gmail.com>
2022-01-20 12:39:11 +0800

    format

--- HeadPoseByMediapipe.py
+++ HeadPoseByMediapipe.py
@@ -10,8 +10,8 @@ mp_drawing_utils.DrawingSpec(thickness=1, circle_radius=1)
 cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 while cam.isOpened():
     success, img = cam.read()
-    cv2.imshow('xxxx', img)
-    if cv2.waitKey(5) & 0xFF == 27: # ESC
+    cv2.imshow("xxxx", img)
+    if cv2.waitKey(5) & 0xFF == 27:  # ESC
         break
 
 cam.release()
